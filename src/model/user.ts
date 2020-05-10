9ba59b99605564c223e84280ae89a31cc95a1323
Estelle Mac <estelleolawinski@MacBook-Pro-de-Estelle-2.local>
2020-05-10 19:06:10 +0200

    first commit

---
+++ src/model/user.ts
@@ -0,0 +1,10 @@
+export class User {
+  name: string;
+  password: string;
+  username: string;
+  website: string;
+  biography: string;
+  email: string;
+  phoneNumber: number;
+  gender: string;
+}
